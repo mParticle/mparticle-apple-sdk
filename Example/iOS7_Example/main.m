cbab07841e4515d8a7e444725b042fcc40160fcf
Dalmo Cirne <dalmo@mparticle.com>
2016-03-10 13:58:12 -0500

    Included a target for iOS 7 in the sample app project

---
+++ Example/iOS7_Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS7_Example
+//
+//  Created by Dalmo Cirne on 3/10/16.
+//  Copyright © 2016 mParticle, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
