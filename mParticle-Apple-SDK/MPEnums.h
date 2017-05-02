2aeb0fb5b8a96f61b3e70844987e58f973ab229f
Alex Pavlov <alex@rushproject.com>
2017-05-02 12:17:23 -0400

    Change instance id to 121

--- mParticle-Apple-SDK/MPEnums.h
+++ mParticle-Apple-SDK/MPEnums.h
@@ -190,7 +190,7 @@ typedef NS_ENUM(NSUInteger, MPKitInstance) {
     /** Kit code for Button */
     MPKitInstanceButton = 1022,
     /** Kit code for Skyhook */
-    MPKitInstanceSkyhook = 120
+    MPKitInstanceSkyhook = 121
 };
 
 /// Log Levels
