ea98256690733c4d8da51cdda89b89c11e1c77c7
Peter Jenkins <pjenkins@mparticle.com>
2017-07-11 17:08:12 -0400

    Add Identities object

--- mParticle-Apple-SDK/Identity/MPIdentityApiRequest.m
+++ mParticle-Apple-SDK/Identity/MPIdentityApiRequest.m
@@ -131,6 +131,262 @@
 //  MPIdentityRequest.m
 //
 
+#import <Foundation/Foundation.h>
+
+//NSString *const kMParticleSDKVersion = @"6.13.3";
+
+typedef NS_ENUM(NSUInteger, MPIdentityType) {
+    MPIdentityTypeAdvertiserId,
+    MPIdentityTypeVendorId,
+    MPIdentityTypePushToken,
+    MPIdentityTypeCustomerId,
+    MPIdentityTypeEmail,
+    MPIdentityTypeFacebook,
+    MPIdentityTypeFacebookCustomAudienceId,
+    MPIdentityTypeGoogle,
+    MPIdentityTypeMicrosoft,
+    MPIdentityTypeOther,
+    MPIdentityTypeTwitter,
+    MPIdentityTypeYahoo
+};
+
+@interface MPIdentities : NSObject
+
+@property (nonatomic) NSString *advertiserId;
+@property (nonatomic) NSString *vendorId;
+@property (nonatomic) NSString *pushToken;
+@property (nonatomic) NSString *customerId;
+@property (nonatomic) NSString *email;
+@property (nonatomic) NSString *facebook;
+@property (nonatomic) NSString *facebookCustomAudienceId;
+@property (nonatomic) NSString *google;
+@property (nonatomic) NSString *microsoft;
+@property (nonatomic) NSString *other;
+@property (nonatomic) NSString *twitter;
+@property (nonatomic) NSString *yahoo;
+
+- (NSDictionary *)dictionaryRepresentation;
+
+@end
+
+@interface MPIdentityClientSDK : NSObject
+
++ (NSDictionary *)clientSDKDictionaryWithVersion:(NSString *)mParticleVersion;
+
+@end
+
+@interface MPIdentityBaseRequest : NSObject
+
+- (NSDictionary *)dictionaryRepresentation;
+
+@end
+
+@implementation MPIdentityBaseRequest
+
+- (NSDictionary *)dictionaryRepresentation {
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    
+    NSDictionary *clientSDKDictionary = [MPIdentityClientSDK clientSDKDictionaryWithVersion:kMParticleSDKVersion];
+    if (clientSDKDictionary) {
+        dictionary[@"client_sdk"] = clientSDKDictionary;
+    }
+    
+    NSString *context = nil;
+    if (context) {
+        dictionary[@"context"] = context;
+    }
+    
+    NSString *environment = nil;
+    if (environment) {
+        dictionary[@"environment"] = environment;
+    }
+    
+    NSString *requestId = nil;
+    if (requestId) {
+        dictionary[@"request_id"] = requestId;
+    }
+    
+    NSNumber *requestTimestamp = nil;
+    if (requestTimestamp) {
+        dictionary[@"request_timestamp_ms"] = requestTimestamp;
+    }
+    
+    return dictionary;
+}
+
+@end
+
+@interface MPIdentifyRequest : MPIdentityBaseRequest
+
+@property (nonatomic) NSString *previousMPID;
+@property (nonatomic) MPIdentities *knownIdentities;
+
+- (NSDictionary *)dictionaryRepresentation;
+
+@end
+
+@interface MPIdentityModifyRequest : MPIdentityBaseRequest
+
+@property (nonatomic) NSArray *identityChanges;
+@property (nonatomic) NSString *mpid;
+
+@end
+
+@implementation MPIdentifyRequest
+
+- (NSDictionary *)dictionaryRepresentation {
+    NSMutableDictionary *dictionary = [[super dictionaryRepresentation] mutableCopy];
+    
+    if (_previousMPID) {
+        dictionary[@"previous_mpid"] = _previousMPID;
+    }
+    
+    NSDictionary *identitiesDictionary = [_knownIdentities dictionaryRepresentation];
+    
+    if (identitiesDictionary) {
+        dictionary[@"known_identities"] = identitiesDictionary;
+    }
+    
+    return dictionary;
+}
+
+@end
+
+
+@implementation MPIdentityClientSDK
+
++ (NSDictionary *)clientSDKDictionaryWithVersion:(NSString *)sdkVersion {
+    if (!sdkVersion) {
+        return nil;
+    }
+    
+#if TARGET_OS_IOS == 1
+    NSString *platform = @"ios";
+#elif TARGET_OS_TVOS == 1
+    NSString *platform = @"tvos";
+#endif
+    
+    NSDictionary *dictionary = @{ @"platform": platform, @"sdk_vendor": @"mparticle", @"sdk_version": sdkVersion };
+    return dictionary;
+}
+
+@end
+
+@implementation MPIdentities
+
+- (instancetype)init {
+    return nil;
+}
+
+- (instancetype)initWithIdentities:(NSDictionary *)identities {
+    if (!identities.count) {
+        return nil;
+    }
+    
+    self = [super init];
+    if (self) {
+        [identities enumerateKeysAndObjectsUsingBlock:^(NSNumber * _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+            MPIdentityType identityType = key.intValue;
+            
+            switch (identityType) {
+                case MPIdentityTypeAdvertiserId:
+                    _advertiserId = obj;
+                    break;
+                    
+                case MPIdentityTypeCustomerId:
+                    _customerId = obj;
+                    break;
+                    
+                case MPIdentityTypeEmail:
+                    _email = obj;
+                    break;
+                    
+                case MPIdentityTypeFacebook:
+                    _facebook = obj;
+                    break;
+                    
+                case MPIdentityTypeFacebookCustomAudienceId:
+                    _facebookCustomAudienceId = obj;
+                    break;
+                    
+                case MPIdentityTypeGoogle:
+                    _google = obj;
+                    break;
+                    
+                case MPIdentityTypeMicrosoft:
+                    _microsoft = obj;
+                    break;
+                    
+                case MPIdentityTypeOther:
+                    _other = obj;
+                    break;
+                    
+                case MPIdentityTypeTwitter:
+                    _twitter = obj;
+                    break;
+                    
+                case MPIdentityTypeYahoo:
+                    _yahoo = obj;
+                    break;
+                    
+                default:
+                    break;
+            }
+        }];
+    }
+    return self;
+}
+
+
+- (NSDictionary *)dictionaryRepresentation {
+    
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    
+    if (_advertiserId) {
+        dictionary[@"ios_idfa"] = _advertiserId;
+    }
+    
+    if (_customerId) {
+        dictionary[@"customerid"] = _customerId;
+    }
+    
+    if (_email) {
+        dictionary[@"email"] = _email;
+    }
+    
+    if (_facebook) {
+        dictionary[@"facebook"] = _facebook;
+    }
+    
+    if (_facebookCustomAudienceId) {
+        dictionary[@"facebookcustomaudienceid"] = _facebookCustomAudienceId;
+    }
+    
+    if (_google) {
+        dictionary[@"google"] = _google;
+    }
+    
+    if (_microsoft) {
+        dictionary[@"microsoft"] = _microsoft;
+    }
+    
+    if (_other) {
+        dictionary[@"other"] = _other;
+    }
+    
+    if (_twitter) {
+        dictionary[@"twitter"] = _twitter;
+    }
+    
+    if (_yahoo) {
+        dictionary[@"yahoo"] = _yahoo;
+    }
+    
+    return dictionary;
+}
+
+@end
+
 @interface MPIdentityRequest : NSObject
 
 @end
@@ -150,21 +406,6 @@
 @implementation MPIdentityRequest
 @end
 
-typedef NS_ENUM(NSUInteger, MPIdentityType) {
-    MPIdentityTypeAdvertiserId,
-    MPIdentityTypeVendorId,
-    MPIdentityTypePushToken,
-    MPIdentityTypeCustomerId,
-    MPIdentityTypeEmail,
-    MPIdentityTypeFacebook,
-    MPIdentityTypeFacebookCustomAudienceId,
-    MPIdentityTypeGoogle,
-    MPIdentityTypeMicrosoft,
-    MPIdentityTypeOther,
-    MPIdentityTypeTwitter,
-    MPIdentityTypeYahoo,
-};
-
 @interface MPIdentityUtils : NSObject
 
 + (NSString *)stringForIdentityType:(MPIdentityType)identityType;
@@ -176,6 +417,8 @@ typedef NS_ENUM(NSUInteger, MPIdentityType) {
 @property (nonatomic) NSString *oldValue;
 @property (nonatomic) NSString *value;
 @property (nonatomic) MPIdentityType identityType;
+
+- (instancetype)initWithOldValue:(NSString *)oldValue value:(NSString *)value identityType:(MPIdentityType)identityType;
 - (NSMutableDictionary *)dictionaryRepresentation;
 
 @end
@@ -292,6 +535,54 @@ typedef NS_ENUM(NSUInteger, MPIdentityType) {
 
 @end
 
+@implementation MPIdentityModifyRequest
+
+- (instancetype)init {
+    return nil;
+}
+
+- (instancetype)initWithMPID:(NSString *)mpid identityChanges:(NSArray *)identityChanges {
+    if (!mpid || !identityChanges.count) {
+        return nil;
+    }
+    
+    self = [super init];
+    if (self) {
+        _identityChanges = identityChanges;
+        _mpid = mpid;
+    }
+    return self;
+}
+
+- (NSDictionary *)dictionaryRepresentation {
+    NSMutableDictionary *dictionary = [[super dictionaryRepresentation] mutableCopy];
+    
+    NSMutableArray *identityChanges = [NSMutableArray array];
+    [_identityChanges enumerateObjectsUsingBlock:^(MPIdentityChange * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSDictionary *changeDictionary = [obj dictionaryRepresentation];
+        [identityChanges addObject:changeDictionary];
+    }];
+    
+    dictionary[@"identity_changes"] = identityChanges;
+    
+    return dictionary;
+}
+
+//- (NSURLRequest *)requestRepresentation {
+//    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+//
+//
+//
+//    dictionary[@"identity_changes"] = @"";
+//
+//    return dictionary;
+//
+////    NSURLSession
+////    dictionary[@"mpid"] = _mpid;
+//}
+
+@end
+
 @implementation MPIdentityUtils
 
 + (NSString *)stringForIdentityType:(MPIdentityType)identityType {
@@ -332,6 +623,3 @@ typedef NS_ENUM(NSUInteger, MPIdentityType) {
 }
 
 @end
-
-
-
