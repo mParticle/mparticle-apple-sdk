0fd3da197ae8021b254acbfd5dc93de9a6d5bf03
Peter Jenkins <pjenkins@mparticle.com>
2017-08-28 11:36:13 -0400

    Delete submodules

--- Scripts/cocoapods_release_kits.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script performs the following action:
-#
-# * Push all the kits to CocoaPods 
-
-set -vx
-git submodule foreach 'pod trunk push $([ "$name" == "Kits/apptentive-kit" ] || [ "$name" == "Kits/revealmobile-kit" ] || echo --use-libraries) --allow-warnings'
