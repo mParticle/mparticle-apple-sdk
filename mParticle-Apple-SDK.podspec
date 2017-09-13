a51bd3159fda4f82d5a71e08e6119d345e305567
mParticle Automation <developers@mparticle.com>
2017-09-13 17:39:56 -0400

    Update version to 6.15.7

--- mParticle-Apple-SDK.podspec
+++ mParticle-Apple-SDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name             = "mParticle-Apple-SDK"
-    s.version          = "6.15.6"
+    s.version          = "6.15.7"
     s.summary          = "mParticle Apple SDK."
 
     s.description      = <<-DESC
--- mParticle-Apple-SDK/MPIConstants.m
+++ mParticle-Apple-SDK/MPIConstants.m
@@ -19,7 +19,7 @@
 #import "MPIConstants.h"
 
 // mParticle SDK Version
-NSString *const kMParticleSDKVersion = @"6.15.6";
+NSString *const kMParticleSDKVersion = @"6.15.7";
 
 // Session Upload Settings
 NSString *const kMPSessionHistoryValue = @"sh";
