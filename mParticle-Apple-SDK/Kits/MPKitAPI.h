1dd7564b54076c43bcc4f90113a6b892f218bc9d
Peter Jenkins <pjenkins@mparticle.com>
2017-09-18 11:17:12 -0400

    Add log methods to the Kit API

--- mParticle-Apple-SDK/Kits/MPKitAPI.h
+++ mParticle-Apple-SDK/Kits/MPKitAPI.h
@@ -2,6 +2,11 @@
 
 @interface MPKitAPI : NSObject
 
+- (void)logError:(NSString *)format, ...;
+- (void)logWarning:(NSString *)format, ...;
+- (void)logDebug:(NSString *)format, ...;
+- (void)logVerbose:(NSString *)format, ...;
+
 - (NSDictionary<NSString *, NSString *> *)integrationAttributes;
 - (NSDictionary<NSNumber *, NSString *> *)userIdentities;
 - (NSDictionary<NSString *, id> *)userAttributes;
