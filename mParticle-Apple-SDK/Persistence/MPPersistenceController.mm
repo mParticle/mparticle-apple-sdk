9f71d7b419ae8a5c13ba1b713a64cf5ecb4254d5
Peter Jenkins <pjenkins@mparticle.com>
2017-11-17 16:06:22 -0500

    Bump DB version to 27

--- mParticle-Apple-SDK/Persistence/MPPersistenceController.mm
+++ mParticle-Apple-SDK/Persistence/MPPersistenceController.mm
@@ -68,7 +68,7 @@ const int MaxBreadcrumbs = 50;
 @synthesize databasePath = _databasePath;
 
 + (void)initialize {
-    databaseVersions = @[@3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14, @15, @16, @17, @18, @19, @20, @21, @22, @23, @24, @25, @26];
+    databaseVersions = @[@3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14, @15, @16, @17, @18, @19, @20, @21, @22, @23, @24, @25, @26, @27];
 }
 
 - (instancetype)init {
