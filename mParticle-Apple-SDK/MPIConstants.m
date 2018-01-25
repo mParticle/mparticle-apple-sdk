17fe91d11cc85d0c3966b2209c252b844dca2693
mParticle Automation <developers@mparticle.com>
2018-01-25 10:22:06 -0500

    Update version to 6.16.0

--- mParticle-Apple-SDK/MPIConstants.m
+++ mParticle-Apple-SDK/MPIConstants.m
@@ -19,7 +19,7 @@
 #import "MPIConstants.h"
 
 // mParticle SDK Version
-NSString *const kMParticleSDKVersion = @"6.15.19";
+NSString *const kMParticleSDKVersion = @"6.16.0";
 
 // Session Upload Settings
 NSString *const kMPSessionHistoryValue = @"sh";
