3518039074c0d834a8f480c43cfa611c3bf1239b
Amisha Chordia <achordia@deloitte.com>
2018-05-16 20:07:17 +0530

    Merge conflicts fixed

--- mParticle-Apple-SDK/Utils/MPIUserDefaults.h
+++ mParticle-Apple-SDK/Utils/MPIUserDefaults.h
@@ -1,31 +1,25 @@
-//
-//  MPIUserDefaults.h
-//
-//  Copyright 2016 mParticle, Inc.
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-//
-
 #import <Foundation/Foundation.h>
+
+@class MPKitConfiguration;
 
 @interface MPIUserDefaults : NSObject
 
 + (nonnull instancetype)standardUserDefaults;
-- (nullable id)mpObjectForKey:(nonnull NSString *)key;
-- (void)setMPObject:(nullable id)value forKey:(nonnull NSString *)key;
+- (nullable id)mpObjectForKey:(nonnull NSString *)key userId:(nonnull NSNumber *)userId;
+- (void)setMPObject:(nullable id)value forKey:(nonnull NSString *)key userId:(nonnull NSNumber *)userId;
+- (void)removeMPObjectForKey:(nonnull NSString *)key userId:(nonnull NSNumber *)userId;
 - (void)removeMPObjectForKey:(nonnull NSString *)key;
 - (nullable id)objectForKeyedSubscript:(nonnull NSString *const)key;
 - (void)setObject:(nullable id)obj forKeyedSubscript:(nonnull NSString *)key;
 - (void)synchronize;
+- (void)migrateUserKeysWithUserId:(nonnull NSNumber *)userId;
+- (void)migrateToSharedGroupIdentifier:(nonnull NSString *)groupIdentifier;
+- (void)migrateFromSharedGroupIdentifier;
+- (nullable NSDictionary *)getConfiguration;
+- (nullable NSArray *)getKitConfigurations;
+- (void)setConfiguration:(nonnull NSDictionary *)responseConfiguration andETag:(nonnull NSString *)eTag;
+- (void)migrateConfiguration;
+- (void)deleteConfiguration;
+- (BOOL)isExistingUserId:(nonnull NSNumber *)userId;
 
 @end
