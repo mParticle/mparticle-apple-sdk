347e5ece666b7623c5c88dee3d44710360a20977
Peter Jenkins <pjenkins@mparticle.com>
2019-10-31 11:48:43 -0400

    Update version to 7.12.5

--- mParticle-Apple-SDK/MPIConstants.m
+++ mParticle-Apple-SDK/MPIConstants.m
@@ -1,7 +1,7 @@
 #import "MPIConstants.h"
 
 // mParticle SDK Version
-NSString *const kMParticleSDKVersion = @"7.12.4";
+NSString *const kMParticleSDKVersion = @"7.12.5";
 
 // Message Type (dt)
 NSString *const kMPMessageTypeKey = @"dt";
