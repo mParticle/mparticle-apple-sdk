737d39bdd06cd723bef4c687c4932a53774e529f
Peter Jenkins <pjenkins@mparticle.com>
2020-12-09 12:41:13 -0500

    Update version to 8.1.1

--- mParticle-Apple-SDK/MPIConstants.m
+++ mParticle-Apple-SDK/MPIConstants.m
@@ -1,7 +1,7 @@
 #import "MPIConstants.h"
 
 // mParticle SDK Version
-NSString *const kMParticleSDKVersion = @"8.1.0";
+NSString *const kMParticleSDKVersion = @"8.1.1";
 
 // Message Type (dt)
 NSString *const kMPMessageTypeKey = @"dt";
