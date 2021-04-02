c492734cdd0fa1aff359ccb4a1f8cb685e977ca2
mParticle Automation <developers@mparticle.com>
2021-04-02 13:49:08 +0000

    Update version to 8.3.0

--- mParticle-Apple-SDK/MPIConstants.m
+++ mParticle-Apple-SDK/MPIConstants.m
@@ -1,7 +1,7 @@
 #import "MPIConstants.h"
 
 // mParticle SDK Version
-NSString *const kMParticleSDKVersion = @"8.2.0";
+NSString *const kMParticleSDKVersion = @"8.3.0";
 
 // Message Type (dt)
 NSString *const kMPMessageTypeKey = @"dt";
