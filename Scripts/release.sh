564c36f259aeabf5d0d70b44862548fb5a9a1bec
Peter Jenkins <pjenkins@mparticle.com>
2021-08-13 10:45:15 -0400

    ci: Specify alternate Carthage xcframework URLs

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -4,7 +4,7 @@ NOTES="$2"
 
 sed -i '' 's/NSString \*const kMParticleSDKVersion = @".*/NSString *const kMParticleSDKVersion = @"'"$VERSION"'";/' mParticle-Apple-SDK/MPIConstants.m
 /usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString $VERSION" Framework/Info.plist
-jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mParticle/mparticle-apple-sdk/releases/download/$PREFIXED_VERSION/mParticle_Apple_SDK.framework.zip"'"}' mParticle_Apple_SDK.json > tmp.json
+jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mParticle/mparticle-apple-sdk/releases/download/$PREFIXED_VERSION/mParticle_Apple_SDK.framework.zip?alt=https://github.com/mParticle/mparticle-apple-sdk/releases/download/$PREFIXED_VERSION/mParticle_Apple_SDK.xcframework.zip"'"}' mParticle_Apple_SDK.json > tmp.json
 mv tmp.json mParticle_Apple_SDK.json
 sudo npm install -g podspec-bump
 podspec-bump -w -i $VERSION
