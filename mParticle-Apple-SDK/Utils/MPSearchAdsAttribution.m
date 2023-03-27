a81693c9a86bfaf22a65cffdd3a1a7535ccef511
Brandon Stalnaker <bstalnaker@mparticle.com>
2023-03-27 10:42:25 -0400

    Perform request async

--- mParticle-Apple-SDK/Utils/MPSearchAdsAttribution.m
+++ mParticle-Apple-SDK/Utils/MPSearchAdsAttribution.m
@@ -52,25 +52,27 @@
             NSURLSession *urlSession = [NSURLSession sessionWithConfiguration:sessionConfiguration
                                                         delegate:nil
                                                    delegateQueue:nil];
-            [urlSession dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *urlResponse, NSError *error) {
-                if (error) {
-                    MPILogError(@"Failed requesting Ads Attribution with error: %@.", [error localizedDescription]);
-                    if (error.code == 1 /* ADClientErrorLimitAdTracking */) {
-                        completionHandler();
-                    } else if ((requestsCompleted + 1) > SEARCH_ADS_ATTRIBUTION_MAX_RETRIES) {
-                        completionHandler();
+            dispatch_async([MParticle messageQueue], ^{
+                [urlSession dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *urlResponse, NSError *error) {
+                    if (error) {
+                        MPILogError(@"Failed requesting Ads Attribution with error: %@.", [error localizedDescription]);
+                        if (error.code == 1 /* ADClientErrorLimitAdTracking */) {
+                            completionHandler();
+                        } else if ((requestsCompleted + 1) > SEARCH_ADS_ATTRIBUTION_MAX_RETRIES) {
+                            completionHandler();
+                        } else {
+                            // Per Apple docs, "Handle any errors you receive and re-poll for data, if required"
+                            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(SEARCH_ADS_ATTRIBUTION_DELAY_BEFORE_RETRY * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                                [self requestAttributionDetailsWithBlock:completionHandler requestsCompleted:(requestsCompleted + 1)];
+                            });
+                        }
                     } else {
-                        // Per Apple docs, "Handle any errors you receive and re-poll for data, if required"
-                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(SEARCH_ADS_ATTRIBUTION_DELAY_BEFORE_RETRY * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                            [self requestAttributionDetailsWithBlock:completionHandler requestsCompleted:(requestsCompleted + 1)];
-                        });
+                        NSDictionary *adAttributionDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                        [MParticle sharedInstance].stateMachine.searchAdsInfo = [[adAttributionDictionary mutableCopy] copy];
+                        completionHandler();
                     }
-                } else {
-                    NSDictionary *adAttributionDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                    [MParticle sharedInstance].stateMachine.searchAdsInfo = [[adAttributionDictionary mutableCopy] copy];
-                    completionHandler();
-                }
-            }];
+                }];
+            });
         }
     } else {
 #if TARGET_OS_IOS == 1 && __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_9_3
