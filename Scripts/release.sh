8e1172a5492dfbd3fdd32d089d2ca86df43618d4
Ben Baron <bbaron@mparticle.com>
2023-03-28 15:31:13 -0500

    changed back repo name now that testing is done

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -12,7 +12,7 @@ sed -i '' 's/NSString \*const kMParticleSDKVersion = @".*/NSString *const kMPart
 /usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString $VERSION" Framework/Info.plist
 
 # Update Carthage release json file
-jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mParticle/mparticle-apple-sdk-semreltest/releases/download/$PREFIXED_VERSION/mParticle_Apple_SDK.framework.zip?alt=https://github.com/mParticle/mparticle-apple-sdk-semreltest/releases/download/$PREFIXED_VERSION/mParticle_Apple_SDK.xcframework.zip"'"}' mParticle_Apple_SDK.json > tmp.json
+jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mParticle/mparticle-apple-sdk/releases/download/$PREFIXED_VERSION/mParticle_Apple_SDK.framework.zip?alt=https://github.com/mParticle/mparticle-apple-sdk/releases/download/$PREFIXED_VERSION/mParticle_Apple_SDK.xcframework.zip"'"}' mParticle_Apple_SDK.json > tmp.json
 mv tmp.json mParticle_Apple_SDK.json
 
 # Update CocoaPods podspec file
@@ -26,11 +26,11 @@ sed -i '' 's/\(^    s.version[^=]*= \).*/\1"'"$VERSION"'"/' mParticle-Apple-SDK.
 # Update SPM package.swift file
 #
 
-SDK_URL="https:\/\/github.com\/mParticle\/mparticle-apple-sdk-semreltest\/releases\/download\/$PREFIXED_VERSION\/mParticle_Apple_SDK.xcframework.zip"
+SDK_URL="https:\/\/github.com\/mParticle\/mparticle-apple-sdk\/releases\/download\/$PREFIXED_VERSION\/mParticle_Apple_SDK.xcframework.zip"
 SDK_CHECKSUM=$(swift package compute-checksum mParticle_Apple_SDK.xcframework.zip)
 sed -i '' 's/\(^let mParticle_Apple_SDK_URL[^=]*= \).*/\1"'"$SDK_URL"'"/' Package.swift
 sed -i '' 's/\(^let mParticle_Apple_SDK_Checksum[^=]*= \).*/\1"'"$SDK_CHECKSUM"'"/' Package.swift
-SDK_URL="https:\/\/github.com\/mParticle\/mparticle-apple-sdk-semreltest\/releases\/download\/$PREFIXED_VERSION\/mParticle_Apple_SDK_NoLocation.xcframework.zip"
+SDK_URL="https:\/\/github.com\/mParticle\/mparticle-apple-sdk\/releases\/download\/$PREFIXED_VERSION\/mParticle_Apple_SDK_NoLocation.xcframework.zip"
 SDK_CHECKSUM=$(swift package compute-checksum mParticle_Apple_SDK_NoLocation.xcframework.zip)
 sed -i '' 's/\(^let mParticle_Apple_SDK_NoLocation_URL[^=]*= \).*/\1"'"$SDK_URL"'"/' Package.swift
 sed -i '' 's/\(^let mParticle_Apple_SDK_NoLocation_Checksum[^=]*= \).*/\1"'"$SDK_CHECKSUM"'"/' Package.swift
