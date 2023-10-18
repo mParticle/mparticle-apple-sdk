80e9d469a68e22900405f539cf743bee53c3f64e
BrandonStalnaker <bstalnaker@mparticle.com>
2023-10-18 11:17:01 -0400

    address swift objective c interoperability

--- mParticle-Apple-SDK/Kits/MPSideloadedKit.swift
+++ mParticle-Apple-SDK/Kits/MPSideloadedKit.swift
@@ -10,69 +10,69 @@ import Foundation
 @objc public class MPSideloadedKit: NSObject {
     @objc public var kitInstance: MPKitProtocol
     
-    internal var eventTypeFilters: [String:Any] = [:]
-    internal var eventNameFilters: [String:Any] = [:]
-    internal var eventAttributeFilters: [String:Any] = [:]
-    internal var messageTypeFilters: [String:Any] = [:]
-    internal var screenNameFilters: [String:Any] = [:]
-    internal var screenAttributeFilters: [String:Any] = [:]
-    internal var userIdentityFilters: [String:Any] = [:]
-    internal var userAttributeFilters: [String:Any] = [:]
-    internal var commerceEventAttributeFilters: [String:Any] = [:]
-    internal var commerceEventEntityTypeFilters: [String:Any] = [:]
-    internal var commerceEventAppFamilyAttributeFilters: [String:Any] = [:]
-    internal var attributeValueFiltering: [String:Any] = [:]
+    private var eventTypeFilters: [String:Any] = [:]
+    private var eventNameFilters: [String:Any] = [:]
+    private var eventAttributeFilters: [String:Any] = [:]
+    private var messageTypeFilters: [String:Any] = [:]
+    private var screenNameFilters: [String:Any] = [:]
+    private var screenAttributeFilters: [String:Any] = [:]
+    private var userIdentityFilters: [String:Any] = [:]
+    private var userAttributeFilters: [String:Any] = [:]
+    private var commerceEventAttributeFilters: [String:Any] = [:]
+    private var commerceEventEntityTypeFilters: [String:Any] = [:]
+    private var commerceEventAppFamilyAttributeFilters: [String:Any] = [:]
+    private var attributeValueFiltering: [String:Any] = [:]
     
     // MUST also include the following keys with empty dictionaries as the values, or the SDK will crash
-    internal var addEventAttributeList: [String:Any] = [:]
-    internal var removeEventAttributeList: [String:Any] = [:]
-    internal var singleItemEventAttributeList: [String:Any] = [:]
+    private var addEventAttributeList: [String:Any] = [:]
+    private var removeEventAttributeList: [String:Any] = [:]
+    private var singleItemEventAttributeList: [String:Any] = [:]
     
     // Consent Filtering being handled seperately
-    internal var consentRegulationFilters: [String:Any] = [:]
-    internal var consentPurposeFilters: [String:Any] = [:]
+    private var consentRegulationFilters: [String:Any] = [:]
+    private var consentPurposeFilters: [String:Any] = [:]
     
     @objc public init(kitInstance: MPKitProtocol) {
         self.kitInstance = kitInstance
     }
     
-    @objc func addEventTypeFilter(eventType: MPEventType) {
+    @objc public func addEventTypeFilter(eventType: MPEventType) {
         eventTypeFilters[MPIHasher.hashEventType(eventType)] = 0
     }
     
-    @objc func addEventNameFilter(eventType: MPEventType, eventName: String) {
+    @objc public func addEventNameFilter(eventType: MPEventType, eventName: String) {
         eventNameFilters[MPIHasher.hashEventName(eventType, eventName: eventName, isLogScreen: false)] = 0
     }
     
-    @objc func addScreenNameFilter(screenName: String) {
+    @objc public func addScreenNameFilter(screenName: String) {
         eventNameFilters[MPIHasher.hashEventName(MPEventType.click, eventName: screenName, isLogScreen: true)] = 0
     }
     
-    @objc func addEventAttributeFilter(eventType: MPEventType, eventName: String, customAttributeKey: String) {
+    @objc public func addEventAttributeFilter(eventType: MPEventType, eventName: String, customAttributeKey: String) {
         eventAttributeFilters[MPIHasher.hashEventAttributeKey(eventType, eventName: eventName, customAttributeName: customAttributeKey, isLogScreen: false)] = 0
     }
     
-    @objc func addScreenAttributeFilter(screenName: String, customAttributeKey: String) {
+    @objc public func addScreenAttributeFilter(screenName: String, customAttributeKey: String) {
         eventAttributeFilters[MPIHasher.hashEventAttributeKey(MPEventType.click, eventName: screenName, customAttributeName: customAttributeKey, isLogScreen: true)] = 0
     }
     
-    @objc func addUserIdentityFilter(userIdentity: MPUserIdentity) {
+    @objc public func addUserIdentityFilter(userIdentity: MPUserIdentity) {
         userIdentityFilters[MPIHasher.hashUserIdentity(userIdentity)] = 0
     }
     
-    @objc func addUserAttributeFilter(userAttributeKey: String) {
+    @objc public func addUserAttributeFilter(userAttributeKey: String) {
         userAttributeFilters[MPIHasher.hashUserAttributeKey(userAttributeKey)] = 0
     }
     
-    @objc func addCommerceEventAttributeFilter(eventType: MPEventType, eventAttributeKey: String) {
+    @objc public func addCommerceEventAttributeFilter(eventType: MPEventType, eventAttributeKey: String) {
         commerceEventAttributeFilters[MPIHasher.hashCommerceEventAttribute(eventType, key: eventAttributeKey)] = 1
     }
     
-    @objc func addCommerceEventEntityTypeFilter(commerceEventKind: MPCommerceEventKind) {
+    @objc public func addCommerceEventEntityTypeFilter(commerceEventKind: MPCommerceEventKind) {
         commerceEventEntityTypeFilters[String(commerceEventKind.rawValue)] = 0
     }
     
-    @objc func addCommerceEventAppFamilyAttributeFilter(attributeKey: String) {
+    @objc public func addCommerceEventAppFamilyAttributeFilter(attributeKey: String) {
         commerceEventAppFamilyAttributeFilters[MPIHasher.hashString(attributeKey.lowercased())] = 1
     }
     
@@ -80,18 +80,18 @@ import Foundation
     // If `forward` is true, ONLY matching events are forwarded, if false, any matching events are blocked
     // NOTE: This is iOS/Android only, web has a different signature
     // Attribute value filtering
-    func setEventAttributeConditionalForwarding(attributeName: String, attributeValue: String, onlyForward: Bool) {
+    @objc public func setEventAttributeConditionalForwarding(attributeName: String, attributeValue: String, onlyForward: Bool) {
         self.attributeValueFiltering["a"] = MPIHasher.hashUserAttributeKey(attributeName)
         self.attributeValueFiltering["v"] = MPIHasher.hashUserAttributeValue(attributeValue)
         self.attributeValueFiltering["i"] = onlyForward;
     }
     
     // Please use the constants starting on line 393 of MPIConstants.h
-    func addMessageTypeFilter(messageTypeConstant: String) {
+    @objc public func addMessageTypeFilter(messageTypeConstant: String) {
         self.messageTypeFilters[messageTypeConstant] = 0
     }
     
-    @objc func getKitConfiguration() -> [String:Any] {
+    @objc public func getKitConfiguration() -> [String:Any] {
         var kitConfig: [String:Any] = [:]
         kitConfig["et"] = self.eventTypeFilters
         kitConfig["ec"] = self.eventNameFilters
