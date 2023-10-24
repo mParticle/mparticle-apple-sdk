ad543fb4875700a8ad6a1b23ad318b17aa6c4533
Brandon Stalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2023-10-24 16:16:18 -0400

    feat: Begin Implementation MPSideloadedKit Filtering Methods (#231)
    * feat: Begin Implementation MPSideloadedKit Filtering Methods
    
    * address swift objective c interoperability

---
+++ mParticle-Apple-SDK/Include/mParticle_Apple_SDK_NoLocation.h
@@ -0,0 +1,17 @@
+//
+//  mParticle_Apple_SDK_NoLocation.h
+//  mParticle-Apple-SDK-NoLocation
+//
+//  Created by Brandon Stalnaker on 10/18/23.
+//
+
+#ifndef mParticle_Apple_SDK_NoLocation_h
+#define mParticle_Apple_SDK_NoLocation_h
+
+#if defined(__has_include) && __has_include(<mParticle_Apple_SDK_NoLocation/mParticle.h>)
+    #import <mParticle_Apple_SDK_NoLocation/mParticle.h>
+#else
+    #import "mParticle.h"
+#endif
+
+#endif /* mParticle_Apple_SDK_NoLocation_h */
