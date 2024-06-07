b2d8b9de112efaf5880a9757df4608db55b36da0
Ben Baron <ben@einsteinx2.com>
2024-06-07 09:22:02 -0500

    fix: Remove deprecated iAd framework

--- mParticle-Apple-SDK/Utils/MPSearchAdsAttribution.m
+++ mParticle-Apple-SDK/Utils/MPSearchAdsAttribution.m
@@ -5,7 +5,6 @@
 #import "MPILogger.h"
 
 #if TARGET_OS_IOS == 1
-#import <iAd/ADClient.h>
 #import <AdServices/AAAttribution.h>
 #endif
 
@@ -21,24 +20,11 @@
 - (void)requestAttributionDetailsWithBlock:(void (^ _Nonnull)(void))completionHandler requestsCompleted:(int)requestsCompleted {
     NSError *error;
     if (@available(iOS 14.3, *)) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        Class MPClientClass = NSClassFromString(@"AAAttribution");
-        if (!MPClientClass) {
-            completionHandler();
-            return;
-        }
-        SEL attributionTokenSelector = NSSelectorFromString(@"attributionTokenWithError:");
-        if (![MPClientClass respondsToSelector:attributionTokenSelector]) {
-            completionHandler();
-            return;
-        }
-        NSString *attributionToken = [MPClientClass performSelector:attributionTokenSelector withObject:error];
+        NSString *attributionToken = [AAAttribution attributionTokenWithError:&error];
         if (!attributionToken) {
             completionHandler();
             return;
         }
-#pragma clang diagnostic pop
         
         if (attributionToken) {
             NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-adservices.apple.com/api/v1/"]];
@@ -88,61 +74,6 @@
                 }];
             });
         }
-    } else {
-#if TARGET_OS_IOS == 1 && __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_9_3
-        if (![MPStateMachine isAppExtension]) {
-            Class MPClientClass = NSClassFromString(@"ADClient");
-            if (!MPClientClass) {
-                completionHandler();
-                return;
-            }
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-            SEL sharedClientSelector = NSSelectorFromString(@"sharedClient");
-            if (![MPClientClass respondsToSelector:sharedClientSelector]) {
-                completionHandler();
-                return;
-            }
-            
-            id MPClientSharedInstance = [MPClientClass performSelector:sharedClientSelector];
-            if (!MPClientSharedInstance) {
-                completionHandler();
-                return;
-            }
-            
-            SEL requestDetailsSelector = NSSelectorFromString(@"requestAttributionDetailsWithBlock:");
-            if (![MPClientSharedInstance respondsToSelector:requestDetailsSelector]) {
-                completionHandler();
-                return;
-            }
-            
-            [MPClientSharedInstance performSelector:requestDetailsSelector withObject:^(NSDictionary *attributionDetails, NSError *error) {
-                dispatch_async([MParticle messageQueue], ^{
-                    
-                    if (attributionDetails && !error) {
-                        [MParticle sharedInstance].stateMachine.searchAdsInfo = [[attributionDetails mutableCopy] copy];
-                        completionHandler();
-                    }
-                    else if (error.code == 1 /* ADClientErrorLimitAdTracking */) {
-                        completionHandler();
-                    }
-                    else if ((requestsCompleted + 1) > SEARCH_ADS_ATTRIBUTION_MAX_RETRIES) {
-                        completionHandler();
-                    } else {
-                        // Per Apple docs, "Handle any errors you receive and re-poll for data, if required"
-                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(SEARCH_ADS_ATTRIBUTION_DELAY_BEFORE_RETRY * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                            [self requestAttributionDetailsWithBlock:completionHandler requestsCompleted:(requestsCompleted + 1)];
-                        });
-                    }
-                });
-            }];
-#pragma clang diagnostic pop
-        } else {
-            completionHandler();
-        }
-#else
-        completionHandler();
-#endif
     }
 }
 
