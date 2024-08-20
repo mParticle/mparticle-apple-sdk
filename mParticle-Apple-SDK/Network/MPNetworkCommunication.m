64f980b0a935455751da28ec0eb569e97506ef2c
Ben Baron <bbaron@mparticle.com>
2024-08-20 09:52:35 -0500

    Update mParticle-Apple-SDK/Network/MPNetworkCommunication.m

--- mParticle-Apple-SDK/Network/MPNetworkCommunication.m
+++ mParticle-Apple-SDK/Network/MPNetworkCommunication.m
@@ -857,7 +857,7 @@ static NSObject<MPConnectorFactoryProtocol> *factory = nil;
         NSObject<MPConnectorResponseProtocol> *response = [connector responseFromPostRequestToURL:mpURL
                                                                                           message:nil
                                                                                  serializedParams:data 
-                                                                                           secret:nil];
+                                                                                           secret:upload.uploadSettings.secret];
         
         NSData *responseData = response.data;
         error = response.error;
