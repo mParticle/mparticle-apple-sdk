f832d21df242ed1fae5dc1fac4366d51d07f7ce5
Brandon Stalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2024-10-15 11:01:16 -0400

    Update mParticle-Apple-SDK/Kits/MPKitContainer.mm
    Co-authored-by: Ben Baron <bbaron@mparticle.com>

--- mParticle-Apple-SDK/Kits/MPKitContainer.mm
+++ mParticle-Apple-SDK/Kits/MPKitContainer.mm
@@ -2307,11 +2307,8 @@ static const NSInteger sideloadedKitCodeStartValue = 1000000000;
                     NSNumber *timestamp = [parameters[1] isKindOfClass:[NSNumber class]] ? (NSNumber*)parameters[1] : nil;
                     execStatus = [kitRegister.wrapperInstance setATTStatus:status withATTStatusTimestampMillis:timestamp];
                 } else if (selector == @selector(setOptOut:)) {
-                    if (parameters.count == 1 && [parameters[0] boolValue]) {
-                        execStatus = [kitRegister.wrapperInstance setOptOut:true];
-                    } else {
-                        execStatus = [kitRegister.wrapperInstance setOptOut:false];
-                    }
+                    BOOL isOptOut = (parameters.count >= 1 && [parameters[0] boolValue]);
+                    execStatus = [kitRegister.wrapperInstance setOptOut:isOptOut];
                 } else if (parameters.count == 3) {
                     typedef MPKitExecStatus *(*send_type)(id, SEL, id, id, id);
                     send_type func = (send_type)objc_msgSend;
