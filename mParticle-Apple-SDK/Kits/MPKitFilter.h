92f5ffe406aa9fbbc3894470649676bbf75ed57c
Mo Mustafa <mmustafa@mparticle.com>
2024-10-16 19:43:32 -0700

    fix: forward original event names to UI for kits with mapped events

--- mParticle-Apple-SDK/Kits/MPKitFilter.h
+++ mParticle-Apple-SDK/Kits/MPKitFilter.h
@@ -11,6 +11,8 @@
 @property (nonatomic, strong, readonly, nullable) NSDictionary *filteredAttributes;
 @property (nonatomic, strong, readonly, nullable) MPCommerceEvent *originalCommerceEvent;
 @property (nonatomic, strong, readonly, nullable) MPBaseEvent *originalEvent;
+@property (nonatomic, strong, readonly, nullable) MPCommerceEvent *originalCommerceEventCopy;
+@property (nonatomic, strong, readonly, nullable) MPBaseEvent *originalEventCopy;
 @property (nonatomic, strong, readonly, nullable) MPCommerceEvent *forwardCommerceEvent;
 @property (nonatomic, strong, readonly, nullable) MPBaseEvent *forwardEvent;
 @property (nonatomic, strong, readonly, nullable) MPConsentState *forwardConsentState;
@@ -19,7 +21,7 @@
 - (nonnull instancetype)initWithFilter:(BOOL)shouldFilter;
 - (nonnull instancetype)initWithFilter:(BOOL)shouldFilter filteredAttributes:(nullable NSDictionary *)filteredAttributes;
 - (nonnull instancetype)initWithEvent:(nonnull MPBaseEvent *)event shouldFilter:(BOOL)shouldFilter;
-- (nonnull instancetype)initWithEvent:(nonnull MPBaseEvent *)event shouldFilter:(BOOL)shouldFilter appliedProjections:(nullable NSArray<MPEventProjection *> *)appliedProjections;
+- (nonnull instancetype)initWithEvent:(nonnull MPBaseEvent *)event shouldFilter:(BOOL)shouldFilter appliedProjections:(nullable NSArray<MPEventProjection *> *)appliedProjections eventCopy:(nullable MPBaseEvent *)eventCopy commerceEventCopy:(nullable MPCommerceEvent *)commerceEventCopy;
 - (nonnull instancetype)initWithCommerceEvent:(nonnull MPCommerceEvent *)commerceEvent shouldFilter:(BOOL)shouldFilter;
 - (nonnull instancetype)initWithCommerceEvent:(nonnull MPCommerceEvent *)commerceEvent shouldFilter:(BOOL)shouldFilter appliedProjections:(nullable NSArray<MPEventProjection *> *)appliedProjections;
 - (nonnull instancetype)initWithConsentState:(nonnull MPConsentState *)state shouldFilter:(BOOL)shouldFilter;
