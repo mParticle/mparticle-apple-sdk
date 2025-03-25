1452c92a2b6bf30ab4020e3c21ac9a4ebe3212cd
Brandon Stalnaker <brandon.stalnaker@rokt.com>
2025-03-25 10:40:20 -0400

    update version constant for tests

--- mParticle-Apple-SDK/MPConstants.swift
+++ mParticle-Apple-SDK/MPConstants.swift
@@ -15,7 +15,7 @@ func MPMilliseconds(timestamp: Double) -> Double {
 // NOTE: I kept the same naming here for clarity, but we should rename these
 //       after we remove them from the MPIConstants.h file
 
-let kMParticleSDKVersion = "8.27.5"
+let kMParticleSDKVersion = "8.28.0"
 
 struct MessageKeys {
     static let kMPMessagesKey = "msgs"
