d5514afab0ff025a6a0cd3963fb0b0b91a4691f2
Denis Chilik <denis.chilik@rokt.com>
2025-08-18 16:19:19 -0400

    - move MPAttributionResult outside mParticle.m - create test MPAttributionResult

---
+++ mParticle-Apple-SDK/MPAttributionResult+MParticlePrivate.m
@@ -0,0 +1,24 @@
+#import <mParticle_Apple_SDK/mParticle.h>
+
+@implementation MPAttributionResult
+
+- (instancetype)initWithKitCode:(NSNumber *) kitCode
+                        kitName:(NSString *) kitName {
+    self = [super init];
+    if (self) {
+        _kitCode = kitCode;
+        _kitName = kitName;
+    }
+    return self;
+}
+
+- (NSString *)description {
+    NSMutableString *description = [[NSMutableString alloc] initWithString:@"MPAttributionResult {\n"];
+    [description appendFormat:@"  kitCode: %@\n", _kitCode];
+    [description appendFormat:@"  kitName: %@\n", _kitName];
+    [description appendFormat:@"  linkInfo: %@\n", _linkInfo];
+    [description appendString:@"}"];
+    return description;
+}
+
+@end
