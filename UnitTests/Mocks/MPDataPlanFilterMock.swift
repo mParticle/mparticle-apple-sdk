3851123cbfcd50d8f0066e4cc8d05fa440d14424
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2025-09-18 14:49:13 -0400

    full test coverage for logCommerceEvent

--- UnitTests/Mocks/MPDataPlanFilterMock.swift
+++ UnitTests/Mocks/MPDataPlanFilterMock.swift
@@ -5,6 +5,7 @@ import mParticle_Apple_SDK_NoLocation
 import mParticle_Apple_SDK
 #endif
 
+@objcMembers
 class MPDataPlanFilterMock: NSObject, MPDataPlanFilterProtocol {
     var isBlockedUserIdentityTypeCalled = false
     var isBlockedUserIdentityTypeUserIdentityTypeParam: MPIdentity?
@@ -44,5 +45,15 @@ class MPDataPlanFilterMock: NSObject, MPDataPlanFilterProtocol {
         transformEventForScreenEventCalled = true
         transformEventForScreenEventScreenEventParam = screenEvent
         return transformEventForScreenEventReturnValue
+    }
+    
+    var transformEventForCommerceEventCalled = false
+    var transformEventForCommerceEventParam: MPCommerceEvent?
+    var transformEventForCommerceEventReturnValue: MPCommerceEvent?
+    
+    func transformEvent(forCommerceEvent commerceEvent: MPCommerceEvent) -> MPCommerceEvent? {
+        transformEventForCommerceEventCalled = true
+        transformEventForCommerceEventParam = commerceEvent
+        return transformEventForCommerceEventReturnValue
     }
 }
