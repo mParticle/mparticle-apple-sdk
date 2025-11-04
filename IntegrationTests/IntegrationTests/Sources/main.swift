d2a6295d9b5f79066a6bcc846ddc3592ac57eb32
Denis Chilik <denis.chilik@rokt.com>
2025-11-04 11:35:30 -0500

    - remove unused code

---
+++ IntegrationTests/IntegrationTests/Sources/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  IntegrationTests
+//
+//  Created by Denis Chilik on 11/4/25.
+//
+
+import Foundation
+
+print("Hello, World!")
--- IntegrationTests/Project.swift
+++ IntegrationTests/Project.swift
@@ -5,33 +5,13 @@ let project = Project(
     targets: [
         .target(
             name: "IntegrationTests",
-            destinations: .iOS,
-            product: .app,
+            destinations: .macOS,
+            product: .commandLineTool,
             bundleId: "dev.tuist.IntegrationTests",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
             buildableFolders: [
-                "IntegrationTests/Sources",
-                "IntegrationTests/Resources",
+                "IntegrationTests/Sources"
             ],
             dependencies: []
-        ),
-        .target(
-            name: "IntegrationTestsTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "dev.tuist.IntegrationTestsTests",
-            infoPlist: .default,
-            buildableFolders: [
-                "IntegrationTests/Tests"
-            ],
-            dependencies: [.target(name: "IntegrationTests")]
-        ),
+        )
     ]
 )
