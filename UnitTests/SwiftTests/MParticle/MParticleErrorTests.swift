3ea7ae975ace3492de1df7fb981b9c11e179da8c
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2025-11-05 10:29:30 -0500

    Organized error tests file with marks

--- UnitTests/SwiftTests/MParticle/MParticleErrorTests.swift
+++ UnitTests/SwiftTests/MParticle/MParticleErrorTests.swift
@@ -14,48 +14,54 @@ import XCTest
 
 final class MParticleErrorTests: MParticleTestBase {
     
+    // MARK: - Properties
+    
     var errorMessage = "error"
+    
+    // MARK: - logErrorCallback
     
     func test_logErrorCallback_logsMessage_whenSuccess() {
         mparticle.logErrorCallback([:], execStatus: .success, message: errorMessage)
-        
         assertReceivedMessage("Logged error with message: \(errorMessage)")
     }
     
     func test_logErrorCallback_doesNotLog_whenFail() {
         mparticle.logErrorCallback([:], execStatus: .fail, message: errorMessage)
-        
         XCTAssertNil(receivedMessage)
     }
     
+    // MARK: - logExceptionCallback
+    
     func test_logExceptionCallback_logsDetails_whenSuccess() {
         mparticle.logExceptionCallback(exception, execStatus: .success, message: "exception", topmostContext: nil)
-        
         assertReceivedMessage("Logged exception name: exception, reason: Test, topmost context: (null)")
     }
     
     func test_logExceptionCallback_doesNotLog_whenFail() {
         mparticle.logExceptionCallback(exception, execStatus: .fail, message: "exception", topmostContext: nil)
-        
         XCTAssertNil(receivedMessage)
     }
+    
+    // MARK: - logCrashCallback
     
     func test_logCrashCallback_logsMessage_whenSuccess() {
         mparticle.logCrashCallback(.success, message: errorMessage)
         assertReceivedMessage("Logged crash with message: \(errorMessage)")
     }
     
+    // MARK: - logNetworkPerformanceCallback
+    
     func test_logNetworkPerformanceCallback_logsMessage_whenSuccess() {
         mparticle.logNetworkPerformanceCallback(.success)
-        
         assertReceivedMessage("Logged network performance measurement")
     }
     
     func test_logNetworkPerformanceCallback_doesNotLog_whenFail() {
         mparticle.logNetworkPerformanceCallback(.fail)
-        
         XCTAssertNil(receivedMessage)
     }
+    
+    // MARK: - logError
     
     func test_logError_withoutEventInfo_invokesBackendWithNil() {
         mparticle.logError(errorMessage)
@@ -63,14 +69,14 @@ final class MParticleErrorTests: MParticleTestBase {
         XCTAssertTrue(executor.executeOnMessageQueueAsync)
         XCTAssertTrue(listenerController.onAPICalledCalled)
         XCTAssertEqual(listenerController.onAPICalledApiName?.description, "logError:eventInfo:")
-        XCTAssertTrue(listenerController.onAPICalledParameter1 as! String == errorMessage)
+        XCTAssertEqual(listenerController.onAPICalledParameter1 as? String, errorMessage)
         
         XCTAssertTrue(backendController.logErrorCalled)
         XCTAssertNil(backendController.logErrorExceptionParam)
         XCTAssertNil(backendController.logErrorTopmostContextParam)
         XCTAssertNil(backendController.logErrorEventInfoParam)
-        backendController.logErrorCompletionHandler?(errorMessage, .success)
         
+        backendController.logErrorCompletionHandler?(errorMessage, .success)
         assertReceivedMessage("Logged error with message: \(errorMessage)")
     }
     
@@ -86,14 +92,14 @@ final class MParticleErrorTests: MParticleTestBase {
         XCTAssertTrue(executor.executeOnMessageQueueAsync)
         XCTAssertTrue(listenerController.onAPICalledCalled)
         XCTAssertEqual(listenerController.onAPICalledApiName?.description, "logError:eventInfo:")
-        XCTAssertTrue(listenerController.onAPICalledParameter1 as! String == errorMessage)
+        XCTAssertEqual(listenerController.onAPICalledParameter1 as? String, errorMessage)
         
         XCTAssertTrue(backendController.logErrorCalled)
         XCTAssertNil(backendController.logErrorExceptionParam)
         XCTAssertNil(backendController.logErrorTopmostContextParam)
-        XCTAssertTrue(backendController.logErrorEventInfoParam as! [String : String] == keyValueDict)
-        backendController.logErrorCompletionHandler?(errorMessage, .success)
+        XCTAssertEqual(backendController.logErrorEventInfoParam as? [String : String], keyValueDict)
         
+        backendController.logErrorCompletionHandler?(errorMessage, .success)
         assertReceivedMessage("Logged error with message: \(errorMessage)")
     }
 }
