470585fe49e057e02f93c006149f61085841afce
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2025-11-07 12:32:57 -0500

    test: add coverage for logError:eventInfo (#448)
    * fully covered logError:eventInfo
    
    * Organized error tests file with marks
    
    * create helper for checking mock listenerController vars

--- UnitTests/SwiftTests/MParticle/MParticleErrorTests.swift
+++ UnitTests/SwiftTests/MParticle/MParticleErrorTests.swift
@@ -14,44 +14,88 @@ import XCTest
 
 final class MParticleErrorTests: MParticleTestBase {
     
+    // MARK: - Properties
+    
+    var errorMessage = "error"
+    
+    // MARK: - logErrorCallback
+    
     func test_logErrorCallback_logsMessage_whenSuccess() {
-        mparticle.logErrorCallback([:], execStatus: .success, message: "error")
-        
-        assertReceivedMessage("Logged error with message: error")
+        mparticle.logErrorCallback([:], execStatus: .success, message: errorMessage)
+        assertReceivedMessage("Logged error with message: \(errorMessage)")
     }
     
     func test_logErrorCallback_doesNotLog_whenFail() {
-        mparticle.logErrorCallback([:], execStatus: .fail, message: "error")
-        
+        mparticle.logErrorCallback([:], execStatus: .fail, message: errorMessage)
         XCTAssertNil(receivedMessage)
     }
     
+    // MARK: - logExceptionCallback
+    
     func test_logExceptionCallback_logsDetails_whenSuccess() {
         mparticle.logExceptionCallback(exception, execStatus: .success, message: "exception", topmostContext: nil)
-        
         assertReceivedMessage("Logged exception name: exception, reason: Test, topmost context: (null)")
     }
     
     func test_logExceptionCallback_doesNotLog_whenFail() {
         mparticle.logExceptionCallback(exception, execStatus: .fail, message: "exception", topmostContext: nil)
-        
         XCTAssertNil(receivedMessage)
     }
     
+    // MARK: - logCrashCallback
+    
     func test_logCrashCallback_logsMessage_whenSuccess() {
-        mparticle.logCrashCallback(.success, message: "Message")
-        assertReceivedMessage("Logged crash with message: Message")
+        mparticle.logCrashCallback(.success, message: errorMessage)
+        assertReceivedMessage("Logged crash with message: \(errorMessage)")
     }
+    
+    // MARK: - logNetworkPerformanceCallback
     
     func test_logNetworkPerformanceCallback_logsMessage_whenSuccess() {
         mparticle.logNetworkPerformanceCallback(.success)
-        
         assertReceivedMessage("Logged network performance measurement")
     }
     
     func test_logNetworkPerformanceCallback_doesNotLog_whenFail() {
         mparticle.logNetworkPerformanceCallback(.fail)
-        
         XCTAssertNil(receivedMessage)
+    }
+    
+    // MARK: - logError
+    
+    func test_logError_withoutEventInfo_invokesBackendWithNil() {
+        mparticle.logError(errorMessage)
+        
+        XCTAssertTrue(executor.executeOnMessageQueueAsync)
+        listenerController.assertCalled(#selector(mparticle.logError(_:eventInfo:)))
+        
+        XCTAssertTrue(backendController.logErrorCalled)
+        XCTAssertNil(backendController.logErrorExceptionParam)
+        XCTAssertNil(backendController.logErrorTopmostContextParam)
+        XCTAssertNil(backendController.logErrorEventInfoParam)
+        
+        backendController.logErrorCompletionHandler?(errorMessage, .success)
+        assertReceivedMessage("Logged error with message: \(errorMessage)")
+    }
+    
+    func test_logError_withNilMessage_logsRequirementMessage() {
+        errorMessage = ""
+        mparticle.logError(errorMessage, eventInfo: keyValueDict)
+        assertReceivedMessage("'message' is required for logError:eventInfo:")
+    }
+    
+    func test_logError_withMessage_invokesBackend() {
+        mparticle.logError(errorMessage, eventInfo: keyValueDict)
+        
+        XCTAssertTrue(executor.executeOnMessageQueueAsync)
+        listenerController.assertCalled(#selector(mparticle.logError(_:eventInfo:)))
+        
+        XCTAssertTrue(backendController.logErrorCalled)
+        XCTAssertNil(backendController.logErrorExceptionParam)
+        XCTAssertNil(backendController.logErrorTopmostContextParam)
+        XCTAssertEqual(backendController.logErrorEventInfoParam as? [String : String], keyValueDict)
+        
+        backendController.logErrorCompletionHandler?(errorMessage, .success)
+        assertReceivedMessage("Logged error with message: \(errorMessage)")
     }
 }
