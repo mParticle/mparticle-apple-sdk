6d8b53909f17ed86b60b10882af168f59a161f3d
Brandon Stalnaker <brandon.stalnaker@rokt.com>
2026-01-14 17:02:29 -0500

    chore: Release 8.41.0

--- Package.swift
+++ Package.swift
@@ -2,12 +2,12 @@
 
 import PackageDescription
 
-let mParticle_Apple_SDK_URL = "https://static.mparticle.com/sdk/ios/v8.40.0/mParticle_Apple_SDK.xcframework.zip"
-let mParticle_Apple_SDK_Checksum = "c7c0846610ad6619b69c4b6fc4e8c64c75169bfa4509edb8fd8d1a8099c7cf25"
+let mParticle_Apple_SDK_URL = "https://static.mparticle.com/sdk/ios/v8.41.0/mParticle_Apple_SDK.xcframework.zip"
+let mParticle_Apple_SDK_Checksum = "9c2550ac04ca41fa4475b43f4b20405560cb7e16bfa4a0205d6d0e96bd3301aa"
 
 let mParticle_Apple_SDK_NoLocation_URL =
     "https://static.mparticle.com/sdk/ios/v8.40.0/mParticle_Apple_SDK_NoLocation.xcframework.zip"
-let mParticle_Apple_SDK_NoLocation_Checksum = "98802a0019064e67418d6bf20ed1fa237cf8492e8fe7b71cfc98d9c0725111fb"
+let mParticle_Apple_SDK_NoLocation_Checksum = "37d974c1b754c2f6a6b156dc05c4573c1860bb26141dc8ffcaaf2834a8dae67d"
 
 let package = Package(
     name: "mParticle-Apple-SDK",
