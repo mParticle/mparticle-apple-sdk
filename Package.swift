9f0ef65ac415228e141f4b16c5b18828632ef323
Brandon Stalnaker <brandon.stalnaker@rokt.com>
2026-01-15 09:21:05 -0500

    fix Integrations Tests

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let mParticle_Apple_SDK_URL = "https://static.mparticle.com/sdk/ios/v8.41.0/mPar
 let mParticle_Apple_SDK_Checksum = "9c2550ac04ca41fa4475b43f4b20405560cb7e16bfa4a0205d6d0e96bd3301aa"
 
 let mParticle_Apple_SDK_NoLocation_URL =
-    "https://static.mparticle.com/sdk/ios/v8.40.0/mParticle_Apple_SDK_NoLocation.xcframework.zip"
+    "https://static.mparticle.com/sdk/ios/v8.41.0/mParticle_Apple_SDK_NoLocation.xcframework.zip"
 let mParticle_Apple_SDK_NoLocation_Checksum = "37d974c1b754c2f6a6b156dc05c4573c1860bb26141dc8ffcaaf2834a8dae67d"
 
 let package = Package(
