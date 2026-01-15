859158f7af5b43a9c93a267f5c275215f4ba1d0f
Denis Chilik <denis.chilik@rokt.com>
2026-01-15 17:36:27 -0500

    - looks like this test wasn't in test target at all, refactored it and add to test target

--- UnitTests/ObjCTests/MPDeviceTests.m
+++ UnitTests/ObjCTests/MPDeviceTests.m
@@ -20,12 +20,24 @@
 
 @implementation MPDeviceTests
 
-- (void)testTelephonyRadioAccessTechnology {
-
-}
-
 - (void)testDictionaryDescription {
-    
+    MParticle *mparticle = [MParticle sharedInstance];
+    MPUserDefaults *userDefaults = [MPUserDefaults
+            standardUserDefaultsWithStateMachine: mparticle.stateMachine
+                               backendController: mparticle.backendController
+                                        identity: mparticle.identity];
+    NSData *testDeviceToken = [@"<000000000000000000000000000000>" dataUsingEncoding:NSUTF8StringEncoding];
+    userDefaults[kMPDeviceTokenKey] = testDeviceToken;
+
+    NSString *testCountry = [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
+
+    MPDevice *device = [[MPDevice alloc] initWithStateMachine: mparticle.stateMachine
+                                                 userDefaults: userDefaults
+                                                     identity: mparticle.identity];
+    NSDictionary *testDictionary = device.dictionaryRepresentation;
+    XCTAssertEqualObjects(testDictionary[@"dll"], @"en");
+    XCTAssertEqualObjects(testDictionary[@"dlc"], testCountry);
+    XCTAssertEqualObjects(testDictionary[@"dma"], @"Apple");
 }
 
 @end
