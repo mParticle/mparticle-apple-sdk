291f815d7902ecb1df761640c2d2d5bc7dda44ea
mParticle Bot User <developers@mparticle.com>
2026-01-20 11:14:16 -0500

    chore: Release v8.41.0 (#504)
    chore(release): 8.41.0
    
    Updates version to 8.41.0 in:
    - CHANGELOG.md
    - Framework/Info.plist
    - Package.swift
    - mParticle-Apple-SDK.podspec
    - mParticle-Apple-SDK/MPConstants.swift
    - mParticle-Apple-SDK/MPIConstants.m
    - mParticle_Apple_SDK.json

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@
 import PackageDescription
 
 let mParticle_Apple_SDK_URL = "https://static.mparticle.com/sdk/ios/v8.41.0/mParticle_Apple_SDK.xcframework.zip"
-let mParticle_Apple_SDK_Checksum = "9c2550ac04ca41fa4475b43f4b20405560cb7e16bfa4a0205d6d0e96bd3301aa"
+let mParticle_Apple_SDK_Checksum = "b7fed3c71b2c91ce43a9df913eff2874e8cdc267f4db0beebb0298a4f8da9d26"
 
 let mParticle_Apple_SDK_NoLocation_URL =
     "https://static.mparticle.com/sdk/ios/v8.41.0/mParticle_Apple_SDK_NoLocation.xcframework.zip"
-let mParticle_Apple_SDK_NoLocation_Checksum = "37d974c1b754c2f6a6b156dc05c4573c1860bb26141dc8ffcaaf2834a8dae67d"
+let mParticle_Apple_SDK_NoLocation_Checksum = "b8316abbbf9620b7d5518de8f933647de5c2b40d643905cae73252910878c7a7"
 
 let package = Package(
     name: "mParticle-Apple-SDK",
