9cdb78a297fac4b75ba772addddef6aadaaa0762
Denis Chilik <denis.chilik@rokt.com>
2026-02-06 15:44:37 -0500

    Remove binary target from Package.swift
    The binary target was causing SPM to attempt downloading and validating
    the binary artifact even when using source-based distribution. Since we're
    using source-based targets exclusively, removing the unused binary target
    resolves the checksum validation error.

--- Package.swift
+++ Package.swift
@@ -2,10 +2,6 @@
 
 import PackageDescription
 
-let mParticle_Apple_SDK_URL =
-    "https://static.mparticle.com/sdk/ios/v8.41.1/mParticle_Apple_SDK.xcframework.zip"
-let mParticle_Apple_SDK_Checksum = "c9aeeb511c407604dd31137e5c714120404ca8d884fd965c4e919b0060d52e12"
-
 let package = Package(
     name: "mParticle-Apple-SDK",
     platforms: [.iOS(.v15), .tvOS(.v15)],
@@ -56,12 +52,6 @@ let package = Package(
                 .linkedFramework("WebKit", .when(platforms: [.iOS])),
                 .linkedFramework("UserNotifications", .when(platforms: [.iOS]))
             ]
-        ),
-        // Binary target (kept for backward compatibility or as alternative)
-        .binaryTarget(
-            name: "mParticle_Apple_SDK_Binary",
-            url: mParticle_Apple_SDK_URL,
-            checksum: mParticle_Apple_SDK_Checksum
         ),
     ]
 )
