5ae58f4a13c3373d01ce382d485368656f422093
mParticle Automation <developers@mparticle.com>
2026-02-11 20:18:55 +0000

    chore(release): 8.42.2
    Updates version to 8.42.2 in:
    - CHANGELOG.md
    - Framework/Info.plist
    - Package.swift
    - mParticle-Apple-SDK.podspec
    - mParticle-Apple-SDK/MPConstants.swift
    - mParticle-Apple-SDK/MPIConstants.m
    - mParticle_Apple_SDK.json
    - IntegrationTests/wiremock-recordings/mappings/*.json

--- Package.swift
+++ Package.swift
@@ -2,12 +2,12 @@
 
 import PackageDescription
 
-let mParticle_Apple_SDK_URL = "https://static.mparticle.com/sdk/ios/v8.42.1/mParticle_Apple_SDK.xcframework.zip"
-let mParticle_Apple_SDK_Checksum = "f31e6f7a624a79c3655a1c256e747d48768edd267d0f810045004401043c0f9c"
+let mParticle_Apple_SDK_URL = "https://static.mparticle.com/sdk/ios/v8.42.2/mParticle_Apple_SDK.xcframework.zip"
+let mParticle_Apple_SDK_Checksum = "2e617c36e0160b1a4e38295287c37c447f16116bc90adbfaf08a70725eb87cc6"
 
 let mParticle_Apple_SDK_NoLocation_URL =
-    "https://static.mparticle.com/sdk/ios/v8.42.1/mParticle_Apple_SDK_NoLocation.xcframework.zip"
-let mParticle_Apple_SDK_NoLocation_Checksum = "2690e0920e361c07798068b9881da6bfaac13dd91a86875cd5564ebdea136d2c"
+    "https://static.mparticle.com/sdk/ios/v8.42.2/mParticle_Apple_SDK_NoLocation.xcframework.zip"
+let mParticle_Apple_SDK_NoLocation_Checksum = "203fbf0584964948e5e23135f038770858abc867694cebb16f8e9b864e34efcc"
 
 let package = Package(
     name: "mParticle-Apple-SDK",
