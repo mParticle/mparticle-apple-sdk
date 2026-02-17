00048592e659dbf44600f5f6bcd10a991116e2a9
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-02-17 13:01:00 -0500

    implement temp dry run and xcframework artifacts

--- Scripts/xcframework.sh
+++ Scripts/xcframework.sh
@@ -2,24 +2,42 @@
 
 #
 # xcframework.sh
-# Paramaters: ./xcframework.sh [scheme]
+# Parameters: ./xcframework.sh [project] [ios_scheme] [tvos_scheme (optional)]
 # Usage examples:
-#   ./xcframework.sh mParticle-Apple-SDK
-#   ./xcframework.sh mParticle-Apple-SDK-NoLocation
+#   ./xcframework.sh mParticle-Apple-SDK.xcodeproj mParticle-Apple-SDK mParticle-Apple-SDK
+#   ./xcframework.sh kits/braze/braze-12/mParticle-Appboy.xcodeproj mParticle-Appboy mParticle-Appboy-tvOS
+#   ./xcframework.sh kits/some-kit/SomeKit.xcodeproj SomeKit
 #
 
 set -euo pipefail
 
-SCHEME=$1
-MODULE=${SCHEME//[-]/_}
+PROJECT=$1
+IOS_SCHEME=$2
+TVOS_SCHEME=${3-}
+MODULE=${IOS_SCHEME//[-]/_}
 
-xcodebuild archive -project mParticle-Apple-SDK.xcodeproj -scheme $SCHEME -destination "generic/platform=iOS" -archivePath "archives/$SCHEME-iOS"
-xcodebuild archive -project mParticle-Apple-SDK.xcodeproj -scheme $SCHEME -destination "generic/platform=iOS Simulator" -archivePath "archives/$SCHEME-iOS_Simulator"
-xcodebuild archive -project mParticle-Apple-SDK.xcodeproj -scheme $SCHEME -destination "generic/platform=tvOS" -archivePath "archives/$SCHEME-tvOS"
-xcodebuild archive -project mParticle-Apple-SDK.xcodeproj -scheme $SCHEME -destination "generic/platform=tvOS Simulator" -archivePath "archives/$SCHEME-tvOS_Simulator"
-xcodebuild -create-xcframework \
-	-archive archives/$SCHEME-iOS.xcarchive -framework $MODULE.framework \
-	-archive archives/$SCHEME-iOS_Simulator.xcarchive -framework $MODULE.framework \
-	-archive archives/$SCHEME-tvOS.xcarchive -framework $MODULE.framework \
-	-archive archives/$SCHEME-tvOS_Simulator.xcarchive -framework $MODULE.framework \
-	-output $MODULE.xcframework
+# iOS (required)
+xcodebuild archive -project "${PROJECT}" -scheme "$IOS_SCHEME" -destination "generic/platform=iOS" -archivePath "archives/$IOS_SCHEME-iOS"
+xcodebuild archive -project "${PROJECT}" -scheme "$IOS_SCHEME" -destination "generic/platform=iOS Simulator" -archivePath "archives/$IOS_SCHEME-iOS_Simulator"
+
+FRAMEWORK_ARGS=(
+	-archive "archives/${IOS_SCHEME}-iOS.xcarchive" -framework "${MODULE}.framework"
+	-archive "archives/${IOS_SCHEME}-iOS_Simulator.xcarchive" -framework "${MODULE}.framework"
+)
+
+# tvOS (optional)
+if [[ -n ${TVOS_SCHEME} ]]; then
+	xcodebuild archive -project "${PROJECT}" -scheme "$TVOS_SCHEME" -destination "generic/platform=tvOS" -archivePath "archives/$TVOS_SCHEME-tvOS"
+	xcodebuild archive -project "${PROJECT}" -scheme "$TVOS_SCHEME" -destination "generic/platform=tvOS Simulator" -archivePath "archives/$TVOS_SCHEME-tvOS_Simulator"
+
+	FRAMEWORK_ARGS+=(
+		-archive "archives/${TVOS_SCHEME}-tvOS.xcarchive" -framework "${MODULE}.framework"
+		-archive "archives/${TVOS_SCHEME}-tvOS_Simulator.xcarchive" -framework "${MODULE}.framework"
+	)
+fi
+
+xcodebuild -create-xcframework "${FRAMEWORK_ARGS[@]}" -output "${MODULE}.xcframework"
+
+codesign --timestamp -s "Apple Distribution: mParticle, inc (DLD43Y3TRP)" "${MODULE}.xcframework"
+zip -r "${MODULE}.xcframework.zip" "${MODULE}.xcframework"
+rm -rf archives "$MODULE.xcframework"
