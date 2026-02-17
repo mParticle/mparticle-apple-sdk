53ebfaf6a6de8e779240e31e8998a09c1e019bb3
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-02-17 14:49:39 -0500

    rename from appboy to braze

---
+++ kits/braze/braze-12/mParticle-Braze-tvOS/mParticle_Braze_tvOS.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for mParticle_Braze_tvOS.
+FOUNDATION_EXPORT double mParticle_Braze_tvOSVersionNumber;
+
+//! Project version string for mParticle_Braze_tvOS.
+FOUNDATION_EXPORT const unsigned char mParticle_Braze_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mParticle_Braze_tvOS/PublicHeader.h>
+
+
+#import <mParticle_Braze/MPKitAppboy.h>
