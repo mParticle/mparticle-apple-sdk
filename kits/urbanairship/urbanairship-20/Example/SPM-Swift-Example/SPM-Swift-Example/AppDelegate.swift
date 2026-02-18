3dfaf70e056107faaea5aa493b37ec3d04abe116
Denis Chilik <denis.chilik@rokt.com>
2026-02-18 12:11:43 -0500

    feat: Add SPM-Swift-Exmple app

---
+++ kits/urbanairship/urbanairship-20/Example/SPM-Swift-Example/SPM-Swift-Example/AppDelegate.swift
@@ -0,0 +1,40 @@
+import UIKit
+import mParticle_Apple_SDK
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let mparticle = MParticle.sharedInstance()
+        mparticle.start(with: .init(
+            key: "REPLACE WITH YOUR MPARTICLE API KEY",
+            secret: "REPLACE WITH YOUR MPARTICLE API SECRET"
+        ))
+        mparticle.logLevel = .verbose
+        if let event = MPEvent(name: "foo", type: .other) {
+            mparticle.logEvent(event)
+        }
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
