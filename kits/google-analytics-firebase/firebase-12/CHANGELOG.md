6a68dcfc2616199749bbbe76646ac0ae89bb2ebf
James Newman <james.newman@rokt.com>
2026-02-19 14:54:58 -0500

    feat: Migrate google-analytics-firebase-12 kit

---
+++ kits/google-analytics-firebase/firebase-12/CHANGELOG.md
@@ -0,0 +1,5 @@
+# mParticle-Firebase Changelog
+
+## 9.0.0
+
+- Initial release supporting Firebase Analytics 12.x
---
+++ kits/google-analytics-firebase/firebase-12/Example/SPM-Swift-Example/SPM-Swift-Example/AppDelegate.swift
@@ -0,0 +1,37 @@
+import UIKit
+import mParticle_Apple_SDK
+import mParticle_Firebase
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let mparticle = MParticle.sharedInstance()
+        mparticle.start(with: .init(
+            key: "REPLACE WITH YOUR MPARTICLE API KEY",
+            secret: "REPLACE WITH YOUR MPARTICLE API SECRET"
+        ))
+        mparticle.logLevel = .verbose
+        if let event = MPEvent(name: "foo", type: .other) {
+            mparticle.logEvent(event)
+        }
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
