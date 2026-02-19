42369809cf5b3d2a6912646d72d61bf5c860bdb6
Denis Chilik <denis.chilik@rokt.com>
2026-02-19 14:15:17 -0500

    Add Obc-ExampleApp

---
+++ kits/appsflyer/appsflyer-6/Example/SPM-Objc-Example/SPM-Objc-Example/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SPM-Objc-Example
+//
+//  Created by Denis Chilik on 2/19/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
