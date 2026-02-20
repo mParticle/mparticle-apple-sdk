9a5ceb9e0c33f89a298c8ac9079253df59e34102
Denis Chilik <denis.chilik@rokt.com>
2026-02-20 11:22:42 -0500

    Add Xcode project

---
+++ kits/kochava/kochava-9/mParticle-Kochava-tvOS/mParticle_Kochava_tvOS.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for mParticle_Kochava_tvOS.
+FOUNDATION_EXPORT double mParticle_Kochava_tvOSVersionNumber;
+
+//! Project version string for mParticle_Kochava_tvOS.
+FOUNDATION_EXPORT const unsigned char mParticle_Kochava_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mParticle_Kochava_tvOS/PublicHeader.h>
+
+
+#import <mParticle_Kochava/MPKitKochava.h>
