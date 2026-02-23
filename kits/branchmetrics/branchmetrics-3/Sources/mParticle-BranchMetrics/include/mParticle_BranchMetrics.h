f904bed4b437210c514d228903354695556b96c5
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-02-23 15:36:43 -0500

    feat: migrate branch v3

---
+++ kits/branchmetrics/branchmetrics-3/Sources/mParticle-BranchMetrics/include/mParticle_BranchMetrics.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for mParticle-BranchMetrics.
+FOUNDATION_EXPORT double mParticle_BranchMetricsVersionNumber;
+
+//! Project version string for mParticle-BranchMetrics.
+FOUNDATION_EXPORT const unsigned char mParticle_BranchMetricsVersionString[];
+
+#import "MPKitBranchMetrics.h"
