b74752177a6026818e1baa5230374e46bb787b43
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-02-23 13:43:26 -0500

    added xcodeprojs

---
+++ kits/rokt/rokt/mParticle-Rokt-tvOS/mParticle_Rokt_tvOS.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for mParticle_Rokt_tvOS.
+FOUNDATION_EXPORT double mParticle_Rokt_tvOSVersionNumber;
+
+//! Project version string for mParticle_Rokt_tvOS.
+FOUNDATION_EXPORT const unsigned char mParticle_Rokt_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mParticle_Rokt_tvOS/PublicHeader.h>
+
+
+#import <mParticle_Rokt/MPKitRokt.h>
