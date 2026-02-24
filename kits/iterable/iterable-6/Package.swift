91445d3a021c03dab06ad2a703ca563e1b2b069e
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-02-24 17:20:38 -0500

    remove tvOS declaration

--- kits/iterable/iterable-6/Package.swift
+++ kits/iterable/iterable-6/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "mParticle-Iterable",
-    platforms: [ .iOS(.v15), .tvOS(.v15) ],
+    platforms: [ .iOS(.v15) ],
     products: [
         .library(
             name: "mParticle-Iterable",
