71e85e9924114d0a28a2c2d8770c288244011fae
Denis Chilik <denis.chilik@rokt.com>
2026-02-26 14:33:41 -0500

    Fix test

--- kits/apptentive/apptentive-6/Tests/mParticle-ApptentiveTests/mParticle_ApptentiveTests.m
+++ kits/apptentive/apptentive-6/Tests/mParticle-ApptentiveTests/mParticle_ApptentiveTests.m
@@ -8,7 +8,15 @@
 
 #import <XCTest/XCTest.h>
 
+#import "MPKitApptentive.h"
 #import "MPKitApptentiveUtils.h"
+
+@interface MPKitApptentive() 
+
+- (NSDictionary *)parseEventInfoDictionary:(NSDictionary *)info;
+@property (assign, nonatomic) BOOL enableTypeDetection;
+
+@end
 
 @interface mParticle_ApptentiveTests : XCTestCase
 
@@ -38,25 +46,28 @@
     };
     
     NSDictionary *expected = @{
-        // boolean
-        @"key-1": @YES,
-        @"key-2": @YES,
-        @"key-3": @NO,
-        @"key-4": @NO,
-
-        // integer
-        @"key-5": @12345,
-        @"key-6": @-12345,
-
-        // double
-        @"key-7": @3.14,
-        @"key-8": @-3.14,
-
-        // string
-        @"key-9": @"123test456",
+        @"key-1": @"true",
+        @"key-2": @"True",
+        @"key-3": @"false",
+        @"key-4": @"False",
+        @"key-1_flag": @YES,
+        @"key-2_flag": @YES,
+        @"key-3_flag": @NO,
+        @"key-4_flag": @NO,
+        @"key-5": @"12345",
+        @"key-5_number": @12345,
+        @"key-6": @"-12345",
+        @"key-6_number": @(-12345),
+        @"key-7": @"3.14",
+        @"key-7_number": @3.14,
+        @"key-8": @"-3.14",
+        @"key-8_number": @(-3.14),
+        @"key-9": @"123test456"
     };
 
-    NSDictionary *actual = MPKitApptentiveParseEventInfo(data);
+    MPKitApptentive *kit = [MPKitApptentive new];
+    kit.enableTypeDetection = YES;
+    NSDictionary *actual = [kit parseEventInfoDictionary:data];
     XCTAssertEqualObjects(expected, actual);
 }
 
