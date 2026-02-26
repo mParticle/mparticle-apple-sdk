092738765eb841637f687af4947d261bfc14abba
Brandon Stalnaker <brandon.stalnaker@rokt.com>
2026-02-26 10:58:01 -0500

    cr: remove NoLocation import

--- kits/apptimize/apptimize-3/Sources/mParticle-Apptimize/include/MPKitApptimize.h
+++ kits/apptimize/apptimize-3/Sources/mParticle-Apptimize/include/MPKitApptimize.h
@@ -1,8 +1,6 @@
 #import <Foundation/Foundation.h>
 #if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle.h>)
     #import <mParticle_Apple_SDK/mParticle.h>
-#elif defined(__has_include) && __has_include(<mParticle_Apple_SDK_NoLocation/mParticle.h>)
-    #import <mParticle_Apple_SDK_NoLocation/mParticle.h>
 #else
     #import "mParticle.h"
 #endif
