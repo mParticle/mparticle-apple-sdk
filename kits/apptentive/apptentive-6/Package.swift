245071d5233a55a4c54cdd918f0941e4323f7411
Denis Chilik <denis.chilik@rokt.com>
2026-02-26 13:52:15 -0500

    Align kit structure

--- kits/apptentive/apptentive-6/Package.swift
+++ kits/apptentive/apptentive-6/Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "mParticle-Apptentive",
-    platforms: [ .iOS(.v13) ],
+    platforms: [ .iOS(.v15) ],
     products: [
         .library(
             name: "mParticle-Apptentive",
-            targets: ["mParticle-Apptentive"]),
+            targets: ["mParticle-Apptentive"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/mParticle/mparticle-apple-sdk",
-                 .upToNextMajor(from: "8.22.0")),
-        .package(url: "https://github.com/apptentive/apptentive-kit-ios",
-                 .upToNextMajor(from: "6.6.0")),
+        .package(
+            url: "https://github.com/mParticle/mparticle-apple-sdk",
+            branch: "workstation/9.0-Release"
+        ),
+        .package(
+            url: "https://github.com/apptentive/apptentive-kit-ios",
+            .upToNextMinor(from: "6.6.0")
+        )
     ],
     targets: [
         .target(
@@ -24,8 +29,14 @@ let package = Package(
                 .product(name: "mParticle-Apple-SDK", package: "mparticle-apple-sdk"),
                 .product(name: "ApptentiveKit", package: "apptentive-kit-ios"),
             ],
-            path: "mParticle-Apptentive",
-            exclude: ["Info.plist"],
             resources: [.process("PrivacyInfo.xcprivacy")],
-            publicHeadersPath: "."),
-    ])
+            publicHeadersPath: "include"
+        ),
+        .testTarget(
+            name: "mParticle-ApptentiveTests",
+            dependencies: [
+                "mParticle-Apptentive"
+            ],
+        )
+    ]
+)
