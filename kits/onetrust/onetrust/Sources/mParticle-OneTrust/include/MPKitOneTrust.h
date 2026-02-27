0c0adab6e737dedfcfdacface09b30ec4a7d1bc0
denischilik <denis.chilik@rokt.com>
2026-02-27 16:54:59 -0500

    feat: Migrate onetrust (#639)
    * feat: Copy onetrust to monorepo
    
    * Remove unsupported files
    
    * Align kit structure
    
    * Add SPM-Objc-Example App
    
    * Add SPM-Swift-Example App
    
    * Remove Comma
    
    * add xcode project
    
    * Update readme and changelog
    
    * add to the matrix
    
    * Rename folder
    
    * fix refference
    
    * remove russian text

---
+++ kits/onetrust/onetrust/Sources/mParticle-OneTrust/include/MPKitOneTrust.h
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle.h>)
+    #import <mParticle_Apple_SDK/mParticle.h>
+#else
+    #import "mParticle.h"
+#endif
+
+@interface MPKitOneTrust : NSObject <MPKitProtocol>
+
+@property (nonatomic, strong, nonnull) NSDictionary *configuration;
+@property (nonatomic, strong, nullable) NSDictionary *launchOptions;
+@property (nonatomic, unsafe_unretained, readonly) BOOL started;
+@property (nonatomic, strong, nullable) NSDictionary *purposeConsentMapping;
+@property (nonatomic, strong, nullable) NSDictionary *venderGeneralConsentMapping;
+@property (nonatomic, strong, nullable) NSDictionary *venderIABConsentMapping;
+@property (nonatomic, strong, nullable) NSDictionary *venderGoogleConsentMapping;
+
+
+@end
