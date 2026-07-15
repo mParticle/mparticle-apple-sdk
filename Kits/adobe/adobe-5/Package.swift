fdb148e3c1cc6e29a06b1f23536941d9fe927d80
Brandon Stalnaker <brandon.stalnaker@rokt.com>
2026-07-15 15:28:10 -0400

    feat: Add tvOS Support to Adobe

--- Kits/adobe/adobe-5/Package.swift
+++ Kits/adobe/adobe-5/Package.swift
@@ -20,7 +20,7 @@ let mParticleAppleSDK: Package.Dependency = {
 
 let package = Package(
     name: "mParticle-Adobe",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v15), .tvOS(.v15)],
     products: [
         .library(
             name: "mParticle-Adobe",
