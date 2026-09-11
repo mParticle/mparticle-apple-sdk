0b3bd226305cc5fc5cf5fbd65ae57475eb193b7c
mParticle Automation <developers@mparticle.com>
2026-09-11 17:42:41 +0000

    chore: (release) 9.5.0
    Updates version to 9.5.0 across the mParticle ecosystem.

--- Kits/rokt/rokt/Sources/mParticle-Rokt/MPKitRokt.m
+++ Kits/rokt/rokt/Sources/mParticle-Rokt/MPKitRokt.m
@@ -3,7 +3,7 @@
 @import RoktContracts;
 
 // Kit version
-static NSString * const kMPRoktKitVersion = @"9.4.3";
+static NSString * const kMPRoktKitVersion = @"9.5.0";
 
 // Constants for kit configuration keys
 static NSString * const kMPKitConfigurationIdKey = @"id";
