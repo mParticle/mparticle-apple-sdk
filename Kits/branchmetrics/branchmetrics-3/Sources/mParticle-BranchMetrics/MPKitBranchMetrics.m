9091de271201e13cf08f4d17d7332ca3a107b1b2
mParticle Bot User <developers@mparticle.com>
2026-09-22 16:02:08 -0400

    chore: Release v9.6.0 (#1036)
    chore: (release) 9.6.0
    
    Updates version to 9.6.0 across the mParticle ecosystem.

--- Kits/branchmetrics/branchmetrics-3/Sources/mParticle-BranchMetrics/MPKitBranchMetrics.m
+++ Kits/branchmetrics/branchmetrics-3/Sources/mParticle-BranchMetrics/MPKitBranchMetrics.m
@@ -145,7 +145,7 @@ NSString *const userIdentificationType = @"userIdentificationType";
             NSString *branchKey = [self.configuration[ekBMAppKey] copy];
             self.branchInstance = [Branch getInstance:branchKey];
 
-            [self.branchInstance registerPluginName:@"mParticleKit" version:@"9.5.1"];
+            [self.branchInstance registerPluginName:@"mParticleKit" version:@"9.6.0"];
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wundeclared-selector"
